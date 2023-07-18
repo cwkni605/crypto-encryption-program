a0241e3ebad3223160b2a7d2634adee9dcbf6ef9
Connor Knight <cwkni605@gmail.com>
2023-07-17 22:42:48 -0700

    removed email functionality
    I removed the packages and the code from the project entirely.

--- passwords.js
+++
@@ -1,4 +0,0 @@
-const CLIENT_ID = '239833299288-attcbt6cg8iu8n8fvvkt2u77i8d54kd2.apps.googleusercontent.com';
-const CLIENT_SECRET = 'FGpJigJM0ORp-G3JewO-AdXL';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground/';
-const REFRESH_TOKEN = '1//04JsW_0UQn8nfCgYIARAAGAQSNwF-L9IrDc20phbP_leTiZUWt4Ejt-jAET5HlDcbxdP5IXmVOSQNx-gOT4UthbWzbwZWkag5Smc';
